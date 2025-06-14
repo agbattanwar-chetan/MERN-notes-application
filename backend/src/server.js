4b6678995333f557ccbd4f12ad21a4e2eebc88a5
chetan <chetanagbattanwar26@gmail.com>
2025-06-14 19:54:22 +0530

    done with backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,13 +8,19 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5001
 
-connectDB()
-
 // middleware
 app.use(express.json()) // This is required to parse req.body
 
+// custom middleware
+// app.use((req, res, next)=>{
+//     console.log(`Req method is ${req.method} and Req URL is ${req.url}`)
+//     next()
+// })
+
 app.use("/api/notes", notesRoutes)
 
-app.listen(PORT, () => {
-    console.log(`server started on PORT:${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server started on PORT:${PORT}`);
+    })
 })
